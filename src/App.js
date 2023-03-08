c6cc54ff8a0f6965052f457afbaa226f2c1aa192
Majomon <mauri.monzon91@gmail.com>
2023-03-08 10:21:17 -0300

    Optimizando

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import "./App.css";
-import Card from "./components/Card/Card.jsx";
 import Cards from "./components/Cards/Cards.jsx";
 import SearchBar from "./components/SearchBar/SearchBar.jsx";
-import characters, { Rick } from "./data.js";
+import characters from "./data.js";
 
 function App() {
   return (
     <div className="App">
       <div className="Header">
-        <img className="imgTitulo" src="https://assets.stickpng.com/images/58f37720a4fa116215a9240f.png"></img>
+        <img className="imgTitulo" src="https://assets.stickpng.com/images/58f37720a4fa116215a9240f.png" alt="imagen"></img>
         <SearchBar onSearch={(characterID) => window.alert(characterID)} />
       </div>
       <div className="Main">
-        <Card
-          name={Rick.name}
-          species={Rick.species}
-          gender={Rick.gender}
-          image={Rick.image}
-          onClose={() => window.alert("Emulamos que se cierra la card")}
-        />
         <Cards characters={characters} />
       </div>
     </div>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,6 +4,8 @@ export default function Card(props) {
    const {name,species,gender,image,onClose}= props
    return (
       <div className={styles.cajaCard}>
+{/*           onClose={() => window.alert("Emulamos que se cierra la card")}
+ */}
          <button className={styles.cardOnClick} onClick={onClose}>X</button>
          <h2 className={styles.cardName}>{name}</h2>
          <h2 className={styles.cardSpecies}>{species}</h2>
