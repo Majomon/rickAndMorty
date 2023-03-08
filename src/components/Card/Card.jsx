820c97b84c222bb07294a43a71382a6caf3e71e9
Majomon <mauri.monzon91@gmail.com>
2023-03-08 01:15:35 -0300

    Primera subida del proyecto integrador

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,14 @@
+import styles from "./Card.module.css"
+
+export default function Card(props) {
+   const {name,species,gender,image,onClose}= props
+   return (
+      <div className={styles.cajaCard}>
+         <button className={styles.cardOnClick} onClick={onClose}>X</button>
+         <h2 className={styles.cardName}>{name}</h2>
+         <h2 className={styles.cardSpecies}>{species}</h2>
+         <h2 className={styles.cardGender}>{gender}</h2>
+         <img className={styles.cardImage} src={image} alt={name} /> 
+      </div>
+   );
+}
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,10 @@
-import Card from './Card';
+import Card from '../Card/Card';
+import styles from "./Cards.module.css"
+
 
 export default function Cards(props) {
    const { characters } = props;
-   return <div>
+   return <div className={styles.cardContainer} >
       {characters.map((character)=>{
          return <Card
             key={character.id}
@@ -10,7 +12,7 @@ export default function Cards(props) {
             species={character.species}
             gender={character.gender}
             image={character.image}
-/*             onClose={()=>{console.log(`Cerrando ${character.name}`);}} */
+            onClose={()=>{console.log(`Cerrando ${character.name}`);}}
          />
       })}
    </div>;
